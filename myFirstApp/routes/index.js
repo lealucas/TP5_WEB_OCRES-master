f77f610d899458e211d82a7e39ece24843f063bc
lealucas <78903864+lealucas@users.noreply.github.com>
2021-10-29 09:53:15 +0200

    q1

--- myFirstApp/routes/index.js
+++ myFirstApp/routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
 
+const hostname='localhost';
+const port = 3000;
+
+const app = express();
+
+app.listen(port, hostname,() => {
+  console.log('Mon serveur fonctionne sur http://${hostname}:${port} \n')
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+/* GET */
+app.get('/test', (req, res)=>{
+  res.json({
+    req: req.method,
+    data: "This is a GET"
+  });
+});
+
+/* PUT */
+app.put('/test', (req, res)=>{
+  res.json({
+    req: req.method,
+    data: "This is a PUT"
+  });
+});
